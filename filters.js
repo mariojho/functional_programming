4825a6ac2a9b54e94cbda2093490bbb5539847ae
Mario Hernández <mario@linuxmex.com>
2018-06-14 23:36:57 +0200

    Create filters.js

---
+++ filters.js
@@ -0,0 +1,10 @@
+var _ = require("lodash")
+
+var numbers = [1, 2, 3, 4, 5]
+var evensFromNumbers = _.filters(nubers, function(element){
+  return element % 2 === 0
+  })
+  
+  console.log(evensFromNumbers)
+  
+  
