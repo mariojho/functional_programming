de7cd19553448c9685521574ec8c13c8fb77bf22
Mario Hernandez <mariojho@gmail.com>
2021-02-22 10:29:35 +0100

    Added semicolon for the employees and lodash

--- filters_string.js
+++ filters_string.js
@@ -1,11 +1,11 @@
-var _ = require(lodash")
+var _ = require("lodash");
 
 var employees = [
   { name: "John", salary: 50000 },
   { name: "Susan", salary: 60000 },
   { name: "Greg", salary: 100000 },
   { name: "Mary", salary: 120000 }
-]
+];
 
 var dueForARise = _.filter(employees, function(employee){
   return employee.salary < 70000
