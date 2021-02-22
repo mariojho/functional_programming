c99a98969c79674d7d0aa6b9541c8e224ca5d18e
mariojho <mario@linuxmex.com>
2021-02-22 21:00:22 +0100

    Fix the code in both cases, mispelling

--- filters_string.js
+++ filters_string.js
@@ -1,16 +1,15 @@
-var _ = require("lodash");
-
+var _ = require('lodash');
 var employees = [
   { name: "John", salary: 50000 },
   { name: "Susan", salary: 60000 },
   { name: "Greg", salary: 100000 },
   { name: "Mary", salary: 120000 }
 ];
-
+_.filter
 var dueForARise = _.filter(employees, function(employee){
   return employee.salary < 70000
 })
 
-console.log(dueForARaise)
+console.log(dueForARise)
 
 //
